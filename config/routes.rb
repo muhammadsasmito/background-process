6be819d552f535e75fda8185058c45f1a0de5404
muhammadsasmito <muhammad.sasmito.a.w@gmail.com>
2018-09-14 15:45:56 +0700

    add some sidekiq config

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
   get "/process", to: "process#index"
   post "/run_process", to: "process#submit_process"
-
+  
   root to: "process#index"
 
 end
